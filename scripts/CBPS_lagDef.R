cca567ef91999567c763e34c5d91e1d3dbb59334
alenc09 <alencar.lucasc@gmail.com>
2022-11-02 12:36:26 +0000

    Create new script to test sarar models with lagged deforestation and finish the lagged GLM models

--- scripts/CBPS_lagDef.R
+++ scripts/CBPS_lagDef.R
@@ -31,7 +31,7 @@ read_xlsx(path = "data/db3cap1_cbps_clean.xlsx") -> data
 #          defPerc_2005 = 100 - nvcPerc_2005,
 #          defPerc_2008 = 100 - nvcPerc_2008) %>%
 #   glimpse -> data2
-writexl::write_xlsx(x= data2, path = "data/db3cap1_cbps_clean.xlsx")
+#writexl::write_xlsx(x= data2, path = "data/db3cap1_cbps_clean.xlsx")
 
 data %>% 
   mutate(defPerc_2010 = 100-nvcPerc_2010) %>% 
@@ -80,224 +80,114 @@ boxplot(bal.covar)
 
 ###Average Treatment Effect----
 ####Fong et al, 2018 method
+####outcome IDHM_E####
+model.idhmE_2005<- glm(data = data[-142,], 
+                       IDHM_E_2010 ~
+                         defPerc_2005 +
+                         I(defPerc_2005^2),
+                       weights = modeldef$weights
+)
+summary(model.idhmE_2005)
+
+####outcome IDHM_L####
+model.idhmL_2005<- glm(data = data[-142,], 
+                       IDHM_L_2010 ~
+                         defPerc_2005 +
+                         I(defPerc_2005^2),
+                       weights = modeldef$weights
+)
+summary(model.idhmL_2005)
+
+####outcome IDHM_R####
+model.idhmR_2005<- glm(data = data[-142,], 
+                       IDHM_R_2010 ~
+                         defPerc_2005 +
+                         I(defPerc_2005^2),
+                       weights = modeldef$weights
+)
+summary(model.idhmR_2005)
+
 ####outcome expov----
 glm(data = data[-142,], 
     expov_2010 ~
       defPerc_2005 +
       I(defPerc_2005^2),
     weights = modeldef$weights
-) -> model.expov10
-summary(model.expov10)
-
-#####Spatial autocorrelation----
-read_municipality(simplified = F)->mun_cat
-data$code_muni<-as.double(data$code_muni)
-inner_join(x = mun_cat, y = data, by = "code_muni")-> mun_cat_data
-poly2nb(mun_cat_data[-142,], queen=TRUE)-> mat_dist2
-nb2listw(mat_dist2)->mat_dist_list
-
-lm.morantest(model.expov10, mat_dist_list, alternative = "two.sided")
-
-sacsarlm(
-  expov_2010 ~
-    defPerc_2010 +
-    I(defPerc_2010 ^ 2) +
-    area_mun +
-    perc_urb_10 +
-    riosProt_06 +
-    irrigPerc_06 +
-    rain_var +
-    percProp_S +
-    pibServpubPC_2010,
-  data = data[-142, ],
-  listw = mat_dist_list,
-  type = "sac"
-) -> m.expov_spat
-summary(m.expov_spat)
-summary(impacts(m.expov_spat,
-                listw = mat_dist_list,
-                R = 500),
-        zstats = TRUE)->summ.sac_expov
-
-
-####outcome IDHM_R####
-model.idhmR_2010<- glm(data = data[-142,], 
-                       IDHM_R_2010 ~
-                         defPerc_2010 +
-                         I(defPerc_2010^2),
-                       weights = modeldef$weights
-)
-summary(model.idhmR_2010)
-
-lm.morantest(model.idhmR_2010,mat_dist_list, alternative = "two.sided")
-
-sacsarlm(
-  IDHM_R_2010 ~
-    defPerc_2010 +
-    I(defPerc_2010 ^ 2) +
-    area_mun +
-    #p_agro_10 +
-    perc_urb_10 +
-    #prodAss_06+
-    #nascProt_06+
-    riosProt_06 +
-    irrigPerc_06 +
-    rain_var +
-    percProp_S +
-    #pibAgroPC_2010+
-    #pibIndPC_2010+
-    pibServpubPC_2010,
-  #carvVeg_10+
-  #lenha_10,
-  data = data[-142, ],
-  listw = mat_dist_list,
-  type = "sac"
-) -> m.idhmR_spat
-summary(m.idhmR_spat)
-summary(impacts(m.idhmR_spat,
-                listw = mat_dist_list,
-                R = 500),
-        zstats = TRUE)->summ.sac_idhmR
-
-
-####outcome IDHM_L####
-model.idhmL_2010<- glm(data = data[-142,], 
-                       IDHM_L_2010 ~
-                         defPerc_2010 +
-                         I(defPerc_2010^2),
-                       weights = modeldef$weights
-)
-summary(model.idhmL_2010)
-
-lm.morantest(model.idhmL_2010,mat_dist_list, alternative = "two.sided")
-
-sacsarlm(
-  IDHM_L_2010 ~
-    defPerc_2010 +
-    I(defPerc_2010 ^ 2) +
-    area_mun +
-    #p_agro_10 +
-    perc_urb_10 +
-    #prodAss_06+
-    #nascProt_06+
-    riosProt_06 +
-    irrigPerc_06 +
-    rain_var +
-    percProp_S +
-    #pibAgroPC_2010+
-    #pibIndPC_2010+
-    pibServpubPC_2010,
-  #carvVeg_10+
-  #lenha_10,
-  data = data[-142, ],
-  listw = mat_dist_list,
-  type = "sac"
-) -> m.idhmL_spat
-
-summary(m.idhmL_spat)
-summary(impacts(m.idhmL_spat,
-                listw = mat_dist_list,
-                R = 500),
-        zstats = TRUE)->summ.sac_idhmL
-
-
-####outcome IDHM_E####
-model.idhmE_2010<- glm(data = data[-142,], 
-                       IDHM_E_2010 ~
-                         defPerc_2010 +
-                         I(defPerc_2010^2),
-                       weights = modeldef$weights
-)
-summary(model.idhmE_2010)
-
-lm.morantest(model.idhmE_2010,mat_dist_list, alternative = "two.sided")
-
-sacsarlm(
-  IDHM_E_2010 ~
-    defPerc_2010 +
-    I(defPerc_2010 ^ 2) +
-    area_mun +
-    perc_urb_10 +
-    riosProt_06 +
-    irrigPerc_06 +
-    rain_var +
-    percProp_S +
-    pibServpubPC_2010,
-  data = data[-142, ],
-  listw = mat_dist_list,
-  type = "sac"
-) -> m.idhmE_spat
-
-summary(m.idhmE_spat)
-summary(impacts(m.idhmE_spat,
-                listw = mat_dist_list,
-                R = 500),
-        zstats = TRUE)->summ.sac_idhmE
-
+) -> model.expov05
+summary(model.expov05)
 
 ####outcome gini####
-model.gini_2010<- glm(data = data[-142,], 
+model.gini_2005<- glm(data = data[-142,], 
                       gini_2010 ~
-                        defPerc_2010 +
-                        I(defPerc_2010^2),
+                        defPerc_2005 +
+                        I(defPerc_2005^2),
                       weights = modeldef$weights
 )
-summary(model.gini_2010)
-
-lm.morantest(model.gini_2010,mat_dist_list, alternative = "two.sided")
-
-sacsarlm(
-  gini_2010 ~
-    defPerc_2010 +
-    I(defPerc_2010 ^ 2) +
-    area_mun +
-    perc_urb_10 +
-    riosProt_06 +
-    irrigPerc_06 +
-    rain_var +
-    percProp_S +
-    pibServpubPC_2010,
-  data = data[-142,],
-  listw = mat_dist_list,
-  type = "sac"
-) -> m.gini_spat
-summary(m.gini_spat)
-summary(impacts(m.gini_spat,
-                listw = mat_dist_list,
-                R = 500),
-        zstats = TRUE)->summ.sac_gini
+summary(model.gini_2005)
 
 
 ####outcome u5mort####
-model.u5mort_2010<- glm(data = data[-142,], 
+model.u5mort_2005<- glm(data = data[-142,], 
                         u5mort_2010 ~
-                          defPerc_2010 +
-                          I(defPerc_2010^2),
+                          defPerc_2005 +
+                          I(defPerc_2005^2),
                         weights = modeldef$weights
 )
-summary(model.u5mort_2010)
+summary(model.u5mort_2005)
 
-lm.morantest(model.u5mort_2010,mat_dist_list, alternative = "two.sided")
 
-sacsarlm(
-  u5mort_2010 ~
-    defPerc_2010 +
-    I(defPerc_2010 ^ 2) +
-    area_mun +
-    perc_urb_10 +
-    riosProt_06 +
-    irrigPerc_06 +
-    rain_var +
-    percProp_S +
-    pibServpubPC_2010,
-  data = data[-142, ],
-  listw = mat_dist_list,
-  type = "sac"
-) -> m.u5mort_spat
-summary(m.u5mort_spat)
-summary(impacts(m.u5mort_spat,
-                listw = mat_dist_list,
-                R = 500),
-        zstats = TRUE)->summ.sac_u5mort
 ##deforestation data 2008----
+####outcome IDHM_E####
+model.idhmE_2008<- glm(data = data[-142,], 
+                       IDHM_E_2010 ~
+                         defPerc_2008 +
+                         I(defPerc_2008^2),
+                       weights = modeldef$weights
+)
+summary(model.idhmE_2008)
 
+####outcome IDHM_L####
+model.idhmL_2008<- glm(data = data[-142,], 
+                       IDHM_L_2010 ~
+                         defPerc_2008 +
+                         I(defPerc_2008^2),
+                       weights = modeldef$weights
+)
+summary(model.idhmL_2008)
+
+####outcome IDHM_R####
+model.idhmR_2008<- glm(data = data[-142,], 
+                       IDHM_R_2010 ~
+                         defPerc_2008 +
+                         I(defPerc_2008^2),
+                       weights = modeldef$weights
+)
+summary(model.idhmR_2008)
+
+####outcome expov----
+glm(data = data[-142,], 
+    expov_2010 ~
+      defPerc_2008 +
+      I(defPerc_2008^2),
+    weights = modeldef$weights
+) -> model.expov08
+summary(model.expov08)
+
+####outcome gini####
+model.gini_2008<- glm(data = data[-142,], 
+                      gini_2010 ~
+                        defPerc_2008 +
+                        I(defPerc_2008^2),
+                      weights = modeldef$weights
+)
+summary(model.gini_2008)
+
+
+####outcome u5mort####
+model.u5mort_2008<- glm(data = data[-142,], 
+                        u5mort_2010 ~
+                          defPerc_2008 +
+                          I(defPerc_2008^2),
+                        weights = modeldef$weights
+)
+summary(model.u5mort_2008)
