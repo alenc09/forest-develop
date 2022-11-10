2a8c0e1d7892b1dce55292f12ad9b21848058191
alenc09 <alencar.lucasc@gmail.com>
2022-11-10 15:47:13 +0000

    advance the longitudinal analysis with lag deforestation data.
    Create the full table data for longitudinal analysis and test it for the first time period using HDI-education. It worked pretty fine, but there are still some improvements I could do to make the script more fluid and lesse repetitive

--- scripts/rmANOVA_lag.R
+++ scripts/rmANOVA_lag.R
@@ -7,6 +7,8 @@ library(here)
 library(dplyr)
 library(stringr)
 library(tidyr)
+library(lme4)
+library(emmeans)
 
 #data----
 read_xlsx(here("data/dbcap1_rma.xlsx"))-> db_rma
@@ -37,9 +39,11 @@ mapb5_cover %>%
   pivot_longer(cols = -code_muni,
                names_to = c(".value", "year"),
                names_pattern = "(.+)_(.+)") %>% 
-  mutate(year = as.factor(year),
-         code_muni = as.factor(code_muni)) %>% 
+  mutate(code_muni = as.factor(code_muni),
+         year = as.double(year)
+         ) %>% 
   select(-area) %>% 
+  filter(year != "mun") %>% 
   mutate(defStage = if_else(condition = defPerc > 0 & defPerc <= 33 ,
                             true = 1,
                             false = if_else(condition = defPerc > 33 & defPerc <= 66,
@@ -49,11 +53,48 @@ mapb5_cover %>%
                                                             false = 0)))) %>%
   glimpse -> db_rma_lag
 
-#Analysis----
-#defStage change----
+db_rma_lag %>% 
+  #filter(year <= 1991) %>% 
+  pivot_wider(names_from = year,
+              names_sep = "_",
+              values_from = 3:11) %>% 
+  #mutate(db_lag_na[,c(9:14)] = gini_1991) %>% 
+  glimpse -> db_lag_na
 
-##IDHM_E----
-lmer(data = , formula = IDHM_E ~ def.stage*year + (1 | code_muni)) -> lmer.idhE
-tab.idhE<- car::Anova(lmer.idhE, type = "II")
-summary(lmer.idhE)
-pw.idhE<- emmeans(lmer.idhE, pairwise ~ def.stage*year, pbkrtest.limit = 3621)
+writexl::write_xlsx(x = db_lag_na, path = "data/db_lag_na.xlsx")
+
+#Need to fill the NA's from lagged deforestation with the reference year for
+#socioeconomic data. Don't know how to do this in R :( so I cheated and used
+#Calc for that. Then I imported the filled data into R.
+
+read_xlsx(path = "data/db_lag_na.xlsx")-> db_lag_full
+
+rma_lag_fun <- function(year1, year2, year3){
+  db_lag_full %>% 
+    pivot_longer(cols = -code_muni,
+                 names_to = c(".value", "year"),
+                 names_pattern = "(.+)_(.+)") %>%
+    na.omit %>% 
+    mutate(code_muni = as.factor(code_muni),
+           year = as.factor(year),
+           defStage = as.factor(defStage)) %>% 
+    filter(year == year1 | year == year2 | year == year3) %>%  
+    glimpse
+}
+
+rma_lag_fun(year1 = 1985, year2 = 1994, year3 = 2004) -> rma_lag_1
+rma_lag_fun(1986, 1995, 2005) -> rma_lag_2
+rma_lag_fun(1987, 1996, 2006) -> rma_lag_3
+rma_lag_fun(1988, 1997, 2007) -> rma_lag_4
+rma_lag_fun(1989, 1998, 2008) -> rma_lag_5
+rma_lag_fun(1990, 1999, 2009) -> rma_lag_6
+rma_lag_fun(1991, 2000, 2010) -> rma_lag_7
+
+#Analysis----
+##group 1 - 1985, 1994, 2004----
+###IDHM_E----
+lmer(data = rma_lag_1, formula = IDHM_E ~ defStage*year + (1 | code_muni)) -> lmer.idhE_1
+tab.idhE_1<- car::Anova(lmer.idhE_1, type = "II")
+summary(lmer.idhE_1)
+pw.idhE_1<- emmeans(lmer.idhE_1, pairwise ~ defStage*year, pbkrtest.limit = 3621)
+
